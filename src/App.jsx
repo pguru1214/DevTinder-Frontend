bd97f887db5977c9d9cbb7f17f3fc77aa3e9c127
gururgp <pguru1214@gmail.com>
2025-11-18 21:22:39 +0530

    login authentication and redirection

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Signup from "./pages/Signup";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./pages/Feed";
+import Profile from "./pages/Profile";
 function App() {
   return (
     <Provider store={appStore}>
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Body />}>
             <Route path="/" element={<Feed />} />
             <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
             <Route path="signup" element={<Signup />} />
           </Route>
         </Routes>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,15 +1,35 @@
+import axios from "axios";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUser } from "../utils/userSlice";
+import { BASE_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user);
+  const dispatch = useDispatch();
+  const handleLogut = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/logout",
+
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeUser(res.data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div>
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link to="/" className="btn btn-ghost text-xl">
+            daisyUI
+          </Link>
         </div>
         {user && (
           <div className="flex gap-2 items-center">
@@ -32,16 +52,16 @@ const NavBar = () => {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
                 </li>
                 <li>
-                  <a>Logout</a>
+                  <button onClick={handleLogut}>Logout</button>
                 </li>
               </ul>
             </div>
